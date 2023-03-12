7d31422033a314c64d6623d515997c1937fc2589
Yiwen789 <luoy2@Carleton.edu>
2023-03-12 12:08:01 -0400

    added useState hook for question form submission

--- app/src/components/AskForm.js
+++ app/src/components/AskForm.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
+import Answer from './Answer';
 
-useState = {}
 function AskForm() {
+  const [submitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+    setIsSubmitted("User asked a question");
+    console.log("submit button clicked");
+  };
+
   return (
     <div>
       <h1>Ask a question</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Question:
-          <input type="text" name="name" />
+          <input type="text" name="question" placeholder="Ask a question" />
         </label>
         <br />
         
         <button type="submit">Submit</button>
       </form>
+      {submitted && <Answer/>}
     </div>
   )
 }
