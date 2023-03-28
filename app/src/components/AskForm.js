d816cfb3ac2036a79b545c2e46226218285e16a5
Yiwen789 <luoy2@Carleton.edu>
2023-03-27 20:46:43 -0400

    created formState object for better state management in AskForm.js

--- app/src/components/AskForm.js
+++ app/src/components/AskForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import API_URL from '../services/api';
@@ -6,31 +6,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 function AskForm() {
-  const [request, setRequest] = useState('');
-  const [requiredFields, setRequiredFields] = useState([]);
-  const [newField, setNewField] = useState(''); // added new state variable
-  const [responses, setResponses] = useState([]);
-  const [answer, setAnswer] = useState('');
-  const [questionsIsLoading, setQuestionsIsLoading] = useState(false);
-  const [resIsLoading, setResIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [showNewField, setShowNewField] = useState(false);
-  const [copied, setCopied] = useState(false);
+  const [formState, setFormState] = useState({
+    request: '',
+    requiredFields: [],
+    newField: '',
+    responses: [],
+    answer: '',
+    questionsIsLoading: false,
+    resIsLoading:false,
+    error: '',
+    showNewField: false,
+    copied: false,
+  });
+
+  // const [request, setRequest] = useState('');
+  // const [requiredFields, setRequiredFields] = useState([]);
+  // const [newField, setNewField] = useState(''); // added new state variable
+  // const [responses, setResponses] = useState([]);
+  // const [answer, setAnswer] = useState('');
+  // const [questionsIsLoading, setQuestionsIsLoading] = useState(false);
+  // const [resIsLoading, setResIsLoading] = useState(false);
+  // const [error, setError] = useState('');
+  // const [showNewField, setShowNewField] = useState(false);
+  // const [copied, setCopied] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!request.trim()) {
-      setError('Please provide a valid request');
+    if (!formState.request.trim()) {
+      setFormState({...formState, error: 'Please provide a valid request'});
       return;
     }
-
-    setQuestionsIsLoading(true);
     // Reformat the original request from user to a prefixed message.
     // TODO: #1 This is a temporary solution. We should use a better way to parse the request.
-    const prefixedMessage = `Return a shortest list of 5 short questions  you need to ask for helping me ${request}. 
+    const prefixedMessage = `Return a shortest list of 5 short questions  you need to ask for helping me ${formState.request}. 
     In the output, put a number in front of each question. `;
 
     try {
+      setFormState({...formState, questionsIsLoading: true});
       const response = await axios.post(`${API_URL}/ask`, { prefixedMessage });
       // Split the response from chatGPT by new line and trim the whitespace.
       // TODO: #2 This is a temporary solution. We may need to adjust the parsing method later. 
@@ -38,67 +50,56 @@ function AskForm() {
       const splitParams = params.map(str => str.split(/^\d+\.\s+/)[1]);
       const additionalInformation = "Additional Information"
       splitParams.push(additionalInformation);
-      console.log(splitParams);
-      setRequiredFields(splitParams);
-      setResponses(Array(splitParams.length).fill(''));
-      setError('');
-    } catch (error) {
-      setError('Oops! Something went wrong. Please try again.');
-    } finally {
-      setQuestionsIsLoading(false);
-    }
+      setFormState({...formState, requiredFields: splitParams, responses: Array(splitParams.length).fill(''), error: '', questionsIsLoading:false});
+    } catch (e) {
+      setFormState({...formState, error: 'Oops! Something went wrong. Please try again.'});
+    } 
   };
 
   // Handle the change of the response input.
   const handleResponseInputChange = (event, index) => {
-    const newResponses = [...responses];
+    const newResponses = [...formState.responses];
     newResponses[index] = event.target.value;
-    setResponses(newResponses);
+    setFormState({...formState, responses: newResponses});
   };
 
   // Add a new field to the requiredFields array.
   const handleAddField = () => {
-
-    if (newField.trim() !== '') { // only add a new field if the input is not empty
-      setRequiredFields([...requiredFields, newField]);
-      setResponses([...responses, '']);
-      setNewField('');
-      setShowNewField(false);
+    if (formState.newField.trim() !== '') { // only add a new field if the input is not empty
+      setFormState({...formState, 
+        requiredFields: [...formState.requiredFields, formState.newField], 
+        responses: [...formState.responses, ''], 
+        newField: '', 
+        showNewField: false});
     }
   };
 
   const handleNewFieldInputChange = (event) => {
-    setNewField(event.target.value); // update the new field input value
+    setFormState({...formState, newField: event.target.value})// update the new field input value
   };
 
   const handleDeleteField = (index) => {
-    const newFields = [...requiredFields];
-    const newResponses = [...responses];
+    const newFields = [...formState.requiredFields];
+    const newResponses = [...formState.responses];
     newFields.splice(index, 1);
     newResponses.splice(index, 1);
-    setRequiredFields(newFields);
-    setResponses(newResponses);
+    setFormState({...formState, requiredFields: newFields, responses: newResponses});
   };
 
   const handleSubmitFields = async (e) => {
     e.preventDefault();
-    setResIsLoading(true);
-    setAnswer('');//clear previous answer
-    const userInputJson = compileToJson(requiredFields, responses);
-    console.log(userInputJson);
+    setFormState({...formState, resIsLoading: true, answer: ''});//clear previous answer
+    const userInputJson = compileToJson(formState.requiredFields, formState.responses);
 
-    const prefixedMessage = `${request},
+    const prefixedMessage = `${formState.request},
     with the following information ${userInputJson}.`;
-    console.log(prefixedMessage);
 
     try {
       const response = await axios.post(`${API_URL}/ask-res`, { prefixedMessage });
-      setAnswer(response.data?.answer);
-    } catch (error) {
-      setError('Oops! Something went wrong. Please try again.');
-    } finally {
-      setResIsLoading(false);
-    }
+      setFormState({...formState, answer: response.data?.answer, resIsLoading: false});
+    } catch (e) {
+      setFormState({...formState, error: 'Oops! Something went wrong. Please try again.'});
+    } 
 
   }
 
@@ -114,21 +115,21 @@ function AskForm() {
   }
 
   function handleCopyClick() {
-    navigator.clipboard.writeText(answer);
-    setCopied(true);
+    navigator.clipboard.writeText(formState.answer);
+    setFormState({...formState, copied:true});
   }
 
   const renderRequiredFields = () => {
-    if (requiredFields.length > 0) {
+    if (formState.requiredFields.length > 0) {
       return (
         <div className="d-flex justify-content-center">
           <div style={{alignItems: ' center', width:'85%'}}>
             <form onSubmit={handleSubmitFields}>
               <div>
-                {requiredFields.map((param, index) => (
+                {formState.requiredFields.map((param, index) => (
                   <div key={index}>
                     <div style={{ display: 'flex', alignItems: ' center', flexDirection: 'row'}}>
-                      {(index !== requiredFields.length - 1)? 
+                      {(index !== formState.requiredFields.length - 1)? 
                         <FontAwesomeIcon
                         icon={faTimesCircle}
                         onClick={() => handleDeleteField(index)}
@@ -139,7 +140,7 @@ function AskForm() {
                     </div>
                     <textarea
                       className="form-control"
-                      value={responses[index] || ''}
+                      value={formState.responses[index] || ''}
                       onChange={(event) => handleResponseInputChange(event, index)}
                       style={{ marginBottom: '10px', width: '85%' }}
                       />
@@ -148,13 +149,13 @@ function AskForm() {
               </div>
               <br />
               <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                <button className="btn btn-success" onClick={() => setShowNewField(true)}>Add a new question</button>
-                {showNewField && (
+                <button className="btn btn-success" onClick={() =>setFormState({...formState, showNewField: true})}>Add a new question</button>
+                {formState.showNewField && (
                   <div className="mt-2">
                     <textarea
                       className="form-control mb-2"
                       cols="50"
-                      value={newField}
+                      value={formState.newField}
                       onChange={handleNewFieldInputChange}
                     />
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -186,7 +187,7 @@ function AskForm() {
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <form onSubmit={handleSubmit} style={{ width: '85%' }}>
-            <textarea className="form-control" placeholder="Type your request here starting with a verb" value={request} onChange={e => setRequest(e.target.value)} />
+            <textarea className="form-control" placeholder="Type your request here starting with a verb" value={formState.request} onChange={e => setFormState({...formState, request: (e.target.value)})} />
             <br />
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
               <button className="btn btn-primary mb-2" type="submit">Send Request</button>
@@ -201,9 +202,9 @@ function AskForm() {
         <div className="column-header">
           <h3>Answer Questions</h3>
         </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {formState.error && <p style={{ color: 'red' }}>{formState.error}</p>}
         <div class="d-flex justify-content-center my-3">
-          {questionsIsLoading && <div class="spinner-border spinner-border-md text-primary"></div>}
+          {formState.questionsIsLoading && <div class="spinner-border spinner-border-md text-primary"></div>}
         </div>
         {renderRequiredFields()}
       </div>
@@ -217,16 +218,16 @@ function AskForm() {
         </div>
         <div class="card-body">
           <div class="d-flex justify-content-center my-3">
-            {resIsLoading && <div class="spinner-border text-primary"></div>}
+            {formState.resIsLoading && <div class="spinner-border text-primary"></div>}
           </div>
           <div class="answer-section">
-            {error && <p class="text-danger">{error}</p>}
+            {formState.error && <p class="text-danger">{formState.error}</p>}
             <div class="answer-text">
-              {answer || ""}
+              {formState.answer || ""}
             </div>
           </div>
           <div class="d-flex justify-content-center my-3">
-            <button class="btn btn-success" onClick={handleCopyClick}>{copied ? 'Copied!' : 'Copy'}</button>
+            <button class="btn btn-success" onClick={handleCopyClick}>{formState.copied ? 'Copied!' : 'Copy'}</button>
           </div>
         </div>
       </div>
